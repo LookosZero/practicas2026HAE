d69d78496d5302d0fc8c34233b89c3c72070c63a
Lookos <lookoszero@gmail.com>
2026-03-29 14:29:09 +0200

    Creacion p7e

---
+++ p7e/p7e.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;p7e.c,1 :: 		void main() {
+;p7e.c,3 :: 		}
+L_end_main:
+	GOTO        $+0
+; end of _main
---
+++ p7e/p7e.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/looko/Desktop/trabajos uni/HAE/practicas2026HAE/p7e/p7e.c"
+void main() {
+
+}
