1a4d6b741de64f9b6a5398e9dfa3abbbdbc27462
Lookos <lookoszero@gmail.com>
2026-03-29 14:35:20 +0200

    Plantilla basica

--- p7e/p7e.asm
+++ p7e/p7e.asm
@@ -1,8 +1,23 @@
 
+_interrupt:
+
+;p7e.c,17 :: 		void interrupt(){
+;p7e.c,19 :: 		}
+L_end_interrupt:
+L__interrupt3:
+	RETFIE      1
+; end of _interrupt
+
 _main:
 
-;p7e.c,1 :: 		void main() {
-;p7e.c,3 :: 		}
+;p7e.c,21 :: 		void main(){
+;p7e.c,22 :: 		Lcd_init();
+	CALL        _Lcd_Init+0, 0
+;p7e.c,24 :: 		while(1){
+L_main0:
+;p7e.c,26 :: 		}
+	GOTO        L_main0
+;p7e.c,27 :: 		}
 L_end_main:
 	GOTO        $+0
 ; end of _main
