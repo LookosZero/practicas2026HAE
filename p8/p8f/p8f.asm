ba4b3567fd352b5cb55156188fd6e71e7c447d73
Lookos <lookoszero@gmail.com>
2026-04-06 15:24:24 +0200

    Folder structure change

---
+++ p8/p8f/p8f.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;p8f.c,1 :: 		void main() {
+;p8f.c,3 :: 		}
+L_end_main:
+	GOTO        $+0
+; end of _main
---
+++ p8/p8f/p8f.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/Lookos/Desktop/Trabajos Uni/practicas2026HAE/p8f/p8f.c"
+void main() {
+
+}
